1f2130a0e3aa3cff54931ee7f0d32770a2446717
Mathew Chong <mathewchong.dev@gmail.com>
2025-03-02 17:12:59 +1100

    Replace context with Redux

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -1,16 +1,19 @@
 import { View, Text } from "react-native";
-import { useContext } from "react";
+// import { useContext } from "react";
 
 import MealsList from "../components/MealsList/MealsList";
-import { FavoritesContext } from "../store/context/favorites-context";
+// import { FavoritesContext } from "../store/context/favorites-context";
 import { MEALS } from "../data/dummy-data";
 import { StyleSheet } from "react-native";
+import { useSelector } from "react-redux";
 
 function FavoritesScreen() {
-  const favoriteMealsCtx = useContext(FavoritesContext);
+  // const favoriteMealsCtx = useContext(FavoritesContext);
+ const favouriteMealIds =  useSelector((state) => state.favoriteMeals.ids)
 
   const favoriteMeals = MEALS.filter((meal) =>
-    favoriteMealsCtx.ids.includes(meal.id)
+    // favoriteMealsCtx.ids.includes(meal.id)
+    favouriteMealIds.includes(meal.id)
   );
 
   if (favoriteMeals.length === 0) {
